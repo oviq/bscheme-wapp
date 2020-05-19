7c63487c643bd8d6a43c7b185676d473c455664a
oviq <mnkisala@gmail.com>
2020-05-19 12:22:40 +0200

    idk really

--- parser.js
+++ parser.js
@@ -2,3 +2,18 @@
 const parse = (text) => {
 	return eval(eval_variables(text))
 }
+
+const to_output = (output, text) => {
+	output.innerHTML += '<div class="out"> ' + text + "</div> \n"
+}
+
+document.getElementById("enter").onclick = () => {
+	let input = document.getElementById("in")
+	let output = document.getElementById("out")
+
+	if (input.value == "") return
+
+	to_output(output, parse(input.value))
+
+	input.value = ""
+}
